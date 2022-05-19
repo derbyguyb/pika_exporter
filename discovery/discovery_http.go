f7740d10aec0558ca381c17d45a2f954914e81c2
guyb <guyb@derbysoft.com>
2022-05-19 17:45:03 +0800

    refactor http discover

--- discovery/discovery_http.go
+++ discovery/discovery_http.go
@@ -10,14 +10,10 @@ import (
 	"time"
 )
 
-var _IllegalResponse = errors.New("Illegal response")
+var invalidResponse = errors.New("invalid response")
 
 func NewHttpDiscovery(httpPath string) (*httpDiscovery, error) {
-	encodedUrl, err := httpUrlEncoded(httpPath)
-	if err != nil {
-		return nil, err
-	}
-	req, err := http.NewRequest("GET", encodedUrl, nil)
+	req, err := newHttpRequest(httpPath)
 	if err != nil {
 		return nil, err
 	}
@@ -25,63 +21,22 @@ func NewHttpDiscovery(httpPath string) (*httpDiscovery, error) {
 	discover := &httpDiscovery{cli: &http.Client{
 		Timeout: 10 * time.Second,
 	}, req: req}
-	err = discover.init()
+
+	err = discover.updateInstances()
 	if err != nil {
 		return nil, err
 	}
 
+	discover.startRegularUpdate()
 	return discover, nil
 }
 
-type httpDiscovery struct {
-	cli       *http.Client
-	req       *http.Request
-	instances []Instance
-}
-
-func (http *httpDiscovery) GetInstances() []Instance {
-	go http.init()
-	return http.instances
-}
-
-func (http *httpDiscovery) init() error {
-	rs, err := http.getResponse()
-	if err != nil {
-		log.Error(err)
-		return err
-	}
-
-	httpJson := &httpJson{}
-	if err = json.Unmarshal(rs, &httpJson); err != nil {
-		log.Error(err)
-		return err
-	}
-
-	if httpJson == nil || httpJson.PikaInstances == nil {
-		log.Error(_IllegalResponse)
-		return _IllegalResponse
-	}
-
-	http.instances = httpJson.PikaInstances
-	return nil
-}
-
-func (http *httpDiscovery) getResponse() ([]byte, error) {
-	resp, err := http.cli.Do(http.req)
-	defer func() {
-		if resp != nil {
-			resp.Body.Close()
-		}
-	}()
-	if err != nil || resp.StatusCode != 200 {
-		return nil, err
-	}
-
-	response, err := ioutil.ReadAll(resp.Body)
+func newHttpRequest(httpPath string) (*http.Request, error) {
+	encodedUrl, err := httpUrlEncoded(httpPath)
 	if err != nil {
 		return nil, err
 	}
-	return response, nil
+	return http.NewRequest("GET", encodedUrl, nil)
 }
 
 func httpUrlEncoded(str string) (string, error) {
@@ -98,6 +53,74 @@ func httpUrlEncoded(str string) (string, error) {
 	}
 	Url.RawQuery = parameters.Encode()
 	return Url.String(), nil
+}
+
+type httpDiscovery struct {
+	cli       *http.Client
+	req       *http.Request
+	instances []Instance
+}
+
+func (http *httpDiscovery) GetInstances() []Instance {
+	return http.instances
+}
+
+func (http *httpDiscovery) startRegularUpdate() {
+	go func() {
+		tickerChan := time.Tick(time.Minute)
+		for {
+			e := http.updateInstances()
+			if e != nil {
+				log.Error("regular update:", e)
+			}
+			<-tickerChan
+		}
+	}()
+}
+
+func (http *httpDiscovery) updateInstances() error {
+	rs, err := http.getResponse()
+	if err != nil {
+		return err
+	}
+
+	httpJson := &httpJson{}
+	if err = json.Unmarshal(rs, &httpJson); err != nil {
+		return err
+	}
+
+	if httpJson.PikaInstances == nil {
+		return invalidResponse
+	}
+
+	http.instances = httpJson.PikaInstances
+	return nil
+}
+
+func (http *httpDiscovery) getResponse() ([]byte, error) {
+	resp, err := http.cli.Do(http.req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if resp != nil {
+			err := resp.Body.Close()
+			if err != nil {
+				log.Error(err)
+			}
+		}
+	}()
+
+	if resp.StatusCode != 200 {
+		return nil, invalidResponse
+	}
+
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
 }
 
 type httpJson struct {
